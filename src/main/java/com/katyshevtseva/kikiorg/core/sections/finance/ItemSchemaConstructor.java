756041f1692a239d90e4151af74d9b70ff1b7fed
Ekaterina Katyshevtseva <e.katyshevtseva@datadrivers.ru>
2020-11-19 10:47:41 +0300

    Закончила бэк для создания редактируемой схемы

--- src/main/java/com/katyshevtseva/kikiorg/core/sections/finance/ItemSchemaConstructor.java
+++ src/main/java/com/katyshevtseva/kikiorg/core/sections/finance/ItemSchemaConstructor.java
@@ -5,15 +5,35 @@ import com.katyshevtseva.kikiorg.core.sections.finance.entity.ItemGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class ItemSchemaConstructor {
     @Autowired
-    private ItemHierarchyService itemHierarchyService;
+    private ItemHierarchyService service;
 
     public List<SchemaLine> getShema() {
-        return null;
+        List<SchemaLine> schema = new ArrayList<>();
+        for (ItemHierarchyNode topLevelNode : service.getTopLevelNodes()) {
+            schema.addAll(getSchemaByRoot(topLevelNode, 0));
+            schema.add(new EmptyLine());
+        }
+        return schema;
+    }
+
+    private List<SchemaLine> getSchemaByRoot(ItemHierarchyNode node, int level) {
+        if (node.isLeaf())
+            return Collections.singletonList(
+                    new Entry(getIndentByLevel(level) + node.getTitle(), getColorByLevel(level), node));
+
+        List<SchemaLine> schema = new ArrayList<>();
+        for (ItemHierarchyNode childNode : service.getNodesByParent(node)) {
+            schema.addAll(getSchemaByRoot(childNode, level + 1));
+        }
+        schema.add(new AddButton(getIndentByLevel(level), (ItemGroup) node));
+        return schema;
     }
 
     private String getIndentByLevel(int level) {
@@ -22,6 +42,16 @@ public class ItemSchemaConstructor {
             indent += "   ";
         }
         return indent;
+    }
+
+    private String getColorByLevel(int level) {
+        if (level == 0)
+            return "#ff0000";
+        if (level == 1)
+            return "#00ff00";
+        if (level == 3)
+            return "#0000ff";
+        return "#000000";
     }
 
     public class Entry implements SchemaLine {
@@ -36,7 +66,15 @@ public class ItemSchemaConstructor {
         }
 
         public void deleteFromShema() {
-            itemHierarchyService.destroyTreeWithRootNode(node);
+            service.destroyTreeWithRootNode(node);
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getColor() {
+            return color;
         }
     }
 
@@ -53,11 +91,11 @@ public class ItemSchemaConstructor {
             if (childNodeToAdd.getParentGroup() != null)
                 throw new SchemaException(childNodeToAdd.getTitle() + " уже имеет родителя");
 
-            if (itemHierarchyService.treeWithRootContainsNode(childNodeToAdd, groupToAddTo))
+            if (service.treeWithRootContainsNode(childNodeToAdd, groupToAddTo))
                 throw new SchemaException(groupToAddTo.getTitle() + " является подкатегорией категории " + childNodeToAdd.getTitle());
 
             childNodeToAdd.setParentGroup(groupToAddTo);
-            itemHierarchyService.saveNode(childNodeToAdd);
+            service.saveNode(childNodeToAdd);
         }
 
         public String getIndent() {
@@ -65,7 +103,7 @@ public class ItemSchemaConstructor {
         }
     }
 
-    public class EmptyLine implements SchemaLine {
+    public static class EmptyLine implements SchemaLine {
 
     }
 
