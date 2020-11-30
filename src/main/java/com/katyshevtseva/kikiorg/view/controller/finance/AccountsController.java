1906dc744118ebee939e2da3b4291e6828a3a81e
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2020-11-30 21:35:26 +0300

    Отображение инфы о счетах в таблице

--- src/main/java/com/katyshevtseva/kikiorg/view/controller/finance/AccountsController.java
+++ src/main/java/com/katyshevtseva/kikiorg/view/controller/finance/AccountsController.java
@@ -1,12 +1,15 @@
 package com.katyshevtseva.kikiorg.view.controller.finance;
 
 import com.katyshevtseva.kikiorg.core.Core;
+import com.katyshevtseva.kikiorg.core.sections.finance.entity.Account;
 import com.katyshevtseva.kikiorg.view.utils.Utils;
 import com.katyshevtseva.kikiorg.view.utils.WindowBuilder.FxController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 
 
 class AccountsController implements FxController {
@@ -19,14 +22,21 @@ class AccountsController implements FxController {
     @FXML
     private Button validationButton;
     @FXML
-    private TextArea reportTextArea;
+    private TableView<Account> table;
+    @FXML
+    private TableColumn<Account, String> titleColumn;
+    @FXML
+    private TableColumn<Account, String> descColumn;
+    @FXML
+    private TableColumn<Account, Long> amountColumn;
 
     @FXML
     private void initialize() {
         addAccountButton.setOnAction(event -> addAccount());
         Utils.associateButtonWithControls(addAccountButton, accountTitleField, accountDescArea);
         validationButton.setOnAction(event -> Core.getInstance().financeService().validateAllAccountsAmount());
-        reportTextArea.setText(Core.getInstance().financeReportService().getReport());
+        adjustColumns();
+        fillTable();
     }
 
     private void addAccount() {
@@ -35,6 +45,27 @@ class AccountsController implements FxController {
                 accountDescArea.getText());
         accountTitleField.clear();
         accountDescArea.setText("-");
-        reportTextArea.setText(Core.getInstance().financeReportService().getReport());
+        fillTable();
+    }
+
+    private void adjustColumns() {
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        descColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        descColumn.setCellFactory(tc -> {
+            TableCell<Account, String> cell = new TableCell<>();
+            Text text = new Text();
+            cell.setGraphic(text);
+            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+            text.wrappingWidthProperty().bind(descColumn.widthProperty());
+            text.textProperty().bind(cell.itemProperty());
+            return cell;
+        });
+    }
+
+    private void fillTable() {
+        ObservableList<Account> accounts = FXCollections.observableArrayList();
+        accounts.addAll(Core.getInstance().financeService().getAccountsForCurrentUser());
+        table.setItems(accounts);
     }
 }
