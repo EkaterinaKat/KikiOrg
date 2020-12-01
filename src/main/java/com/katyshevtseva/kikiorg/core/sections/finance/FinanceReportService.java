9fe625cf553b63b1eb258148fffd7c5674c4bfb6
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2020-12-01 22:52:00 +0300

    Бэк для раздела отчета по финансам

--- src/main/java/com/katyshevtseva/kikiorg/core/sections/finance/FinanceReportService.java
+++ src/main/java/com/katyshevtseva/kikiorg/core/sections/finance/FinanceReportService.java
@@ -3,11 +3,13 @@ package com.katyshevtseva.kikiorg.core.sections.finance;
 import com.katyshevtseva.kikiorg.core.date.DateEntity;
 import com.katyshevtseva.kikiorg.core.date.DateService;
 import com.katyshevtseva.kikiorg.core.repo.ExpenseRepo;
+import com.katyshevtseva.kikiorg.core.sections.finance.ItemHierarchyService.ItemHierarchyNode;
 import com.katyshevtseva.kikiorg.core.sections.finance.entity.Expense;
 import com.katyshevtseva.kikiorg.core.sections.finance.entity.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,12 +22,40 @@ public class FinanceReportService {
     @Autowired
     private ExpenseRepo expenseRepo;
 
-    private long getAmountByNodeAndPeriod(ItemHierarchyService.ItemHierarchyNode node, Date startDate, Date endDate) {
+    public List<ExpensesSegment> getHeadReport(Date startDate, Date endDate) {
+        List<ItemHierarchyNode> nodes = itemHierarchyService.getTopLevelNodesForCurrentUser();
+        return getReportByNodes(nodes, startDate, endDate);
+    }
+
+    public List<ExpensesSegment> getReportByRoot(ItemHierarchyNode root, Date startDate, Date endDate) {
+        List<ItemHierarchyNode> nodes = itemHierarchyService.getNodesByParentForCurrentUser(root);
+        return getReportByNodes(nodes, startDate, endDate);
+    }
+
+    private List<ExpensesSegment> getReportByNodes(List<ItemHierarchyNode> nodes, Date startDate, Date endDate) {
+        List<ExpensesSegment> segments = new ArrayList<>();
+        for (ItemHierarchyNode node : nodes) {
+            segments.add(new ExpensesSegment(node, getAmountByNodeAndPeriod(node, startDate, endDate)));
+        }
+        setSegmentsPercents(segments);
+        return segments;
+    }
+
+    private void setSegmentsPercents(List<ExpensesSegment> segments) {
+        long total = 0;
+        for (ExpensesSegment segment : segments)
+            total += segment.getAmount();
+        for (ExpensesSegment segment : segments) {
+            segment.setPercent((int) ((segment.getAmount() * 100) / total));
+        }
+    }
+
+    private long getAmountByNodeAndPeriod(ItemHierarchyNode node, Date startDate, Date endDate) {
         if (node instanceof Item)
             return getAmountByItemAndPeriod((Item) node, startDate, endDate);
 
         long amount = 0;
-        for (ItemHierarchyService.ItemHierarchyNode childNode : itemHierarchyService.getNodesByParentForCurrentUser(node))
+        for (ItemHierarchyNode childNode : itemHierarchyService.getNodesByParentForCurrentUser(node))
             amount += getAmountByNodeAndPeriod(childNode, startDate, endDate);
         return amount;
     }
@@ -40,5 +70,32 @@ public class FinanceReportService {
             }
         }
         return amount;
+    }
+
+    public class ExpensesSegment {
+        private ItemHierarchyNode node;
+        private int percent;
+        private long amount;
+
+        ExpensesSegment(ItemHierarchyNode node, long amount) {
+            this.node = node;
+            this.amount = amount;
+        }
+
+        public ItemHierarchyNode getNode() {
+            return node;
+        }
+
+        public int getPercent() {
+            return percent;
+        }
+
+        public long getAmount() {
+            return amount;
+        }
+
+        void setPercent(int percent) {
+            this.percent = percent;
+        }
     }
 }
