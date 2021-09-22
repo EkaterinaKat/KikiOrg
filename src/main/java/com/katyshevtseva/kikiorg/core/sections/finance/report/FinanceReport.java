36fab7b55d090561aab2b8c62ba78bf41b28a9d7
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2021-09-22 15:12:16 +0300

    FIN-12

--- src/main/java/com/katyshevtseva/kikiorg/core/sections/finance/report/FinanceReport.java
+++ src/main/java/com/katyshevtseva/kikiorg/core/sections/finance/report/FinanceReport.java
@@ -10,6 +10,9 @@ import java.util.List;
 public class FinanceReport {
     private final String title;
     private final List<Line> lines = new ArrayList<>();
+    private Long total;
+    private List<List<ReportCell>> table;
+    private PieChartData pieChartData;
 
     public FinanceReport(String title) {
         this.title = title;
@@ -17,22 +20,61 @@ public class FinanceReport {
 
     public void addLine(String title, Long value) {
         lines.add(new Line(title, value, null));
+        resetCalculatedData();
     }
 
     public void addLine(String title, Long value, ItemGroup itemGroup) {
         lines.add(new Line(title, value, itemGroup));
+        resetCalculatedData();
+    }
+
+    private void resetCalculatedData() {
+        total = null;
+        table = null;
+        pieChartData = null;
     }
 
     public List<List<ReportCell>> getTable() {
-        return null;
+        if (table == null)
+            initializeTable();
+        return table;
     }
 
     public PieChartData getPieChartData() {
-        return null;
+        if (pieChartData == null)
+            initializePieChartData();
+        return pieChartData;
     }
 
     public Long getTotal() {
-        return lines.stream().map(line -> line.value).reduce(Long::sum).orElse(0L);
+        if (total == null)
+            total = lines.stream().map(line -> line.value).reduce(Long::sum).orElse(0L);
+        return total;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void initializeTable() {
+        if (pieChartData == null)
+            initializePieChartData();
+
+        table = new ArrayList<>();
+        for (PieChartData.Segment segment : pieChartData.getGetSegmentList()) {
+            List<ReportCell> line = new ArrayList<>();
+            line.add(ReportCell.filled(segment.getTitle(), ReportCell.Color.WHITE, 200));
+            line.add(ReportCell.filled(segment.getAmount() + "", ReportCell.Color.WHITE, 100));
+            line.add(ReportCell.filled(segment.getPercent() + "", ReportCell.Color.WHITE, 100));
+            table.add(line);
+        }
+    }
+
+    private void initializePieChartData() {
+        PieChartData pieChartData = new PieChartData(title);
+        for (Line line : lines) {
+            pieChartData.addSegment(new PieChartData.Segment(line.value.intValue(), line.title, null)); //todo
+        }
     }
 
     static class Line {
