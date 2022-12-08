b80246e32efdaf1e10581659cdf45c836b03b75c
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2022-12-08 20:36:42 +0300

    Внешний вид таблицы активностей активностей

--- src/main/java/com/katyshevtseva/kikiorg/view/controller/structure/ActivitiesController.java
+++ src/main/java/com/katyshevtseva/kikiorg/view/controller/structure/ActivitiesController.java
@@ -1,5 +1,8 @@
 package com.katyshevtseva.kikiorg.view.controller.structure;
 
+import com.katyshevtseva.fx.Styler;
+import com.katyshevtseva.fx.Styler.StandardColor;
+import com.katyshevtseva.fx.Styler.ThingToColor;
 import com.katyshevtseva.fx.WindowBuilder.FxController;
 import com.katyshevtseva.fx.dialogconstructor.DcTextField;
 import com.katyshevtseva.fx.dialogconstructor.DialogConstructor;
@@ -9,14 +12,19 @@ import com.katyshevtseva.kikiorg.core.sections.structure.entity.Param;
 import com.katyshevtseva.kikiorg.core.sections.structure.entity.ParamValue;
 import com.katyshevtseva.kikiorg.view.utils.OrganizerWindowCreator;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static com.katyshevtseva.fx.Styler.StandardColor.*;
 
 public class ActivitiesController implements FxController {
     @FXML
@@ -44,23 +52,48 @@ public class ActivitiesController implements FxController {
         for (int k = 0; k <= indexActivityMap.size(); k++) {
             for (int l = 0; l <= indexParamMap.size(); l++) {
                 if (k == 0 && l == 0) {
-                    gridPane.add(new Label("Activities"), l, k);
+                    gridPane.add(getHeadlineNode("Activities"), l, k);
                 } else if (k == 0) {
-                    gridPane.add(new Label(indexParamMap.get(l).getTitle()), l, k);
+                    gridPane.add(getHeadlineNode(indexParamMap.get(l).getTitle()), l, k);
                 } else if (l == 0) {
-                    gridPane.add(new Label(indexActivityMap.get(k).getTitle()), l, k);
+                    gridPane.add(getActivityNode(indexActivityMap.get(k).getTitle()), l, k);
                 } else {
                     Activity activity = indexActivityMap.get(k);
                     Param param = indexParamMap.get(l);
-                    Label label = new Label(getValuesString(activity, param));
-                    label.setOnMouseClicked(event -> OrganizerWindowCreator.getInstance().openValuesSelectDialog(
+                    Node node = getRegularNode(getValuesString(activity, param));
+                    node.setOnMouseClicked(event -> OrganizerWindowCreator.getInstance().openValuesSelectDialog(
                             new ValuesSelectController(param, activity, this::fillPane)
                     ));
-                    gridPane.add(label, l, k);
+                    gridPane.add(node, l, k);
                 }
             }
         }
+    }
 
+    private Node getHeadlineNode(String s) {
+        return getTableCell(s, 20, WHITE, BLACK, WHITE);
+    }
+
+    private Node getActivityNode(String s) {
+        return getTableCell(s, 15, BLACK, PASTEL_PINK, GRAY);
+    }
+
+    private Node getRegularNode(String s) {
+        return getTableCell(s, 15, BLACK, WHITE, GRAY);
+    }
+
+    private Node getTableCell(String text, int textSize, StandardColor textColor, StandardColor backgroundColor,
+                              StandardColor borderColor) {
+        Label label = new Label(text);
+        label.setStyle(Styler.getColorfullStyle(ThingToColor.TEXT, textColor) +
+                Styler.getTextSizeStyle(textSize));
+        label.setPadding(new Insets(textSize));
+
+        Pane pane = new Pane();
+        pane.setStyle(Styler.getColorfullStyle(ThingToColor.BACKGROUND, backgroundColor) +
+                Styler.getColorfullStyle(ThingToColor.BORDER, borderColor));
+        pane.getChildren().add(label);
+        return pane;
     }
 
     private String getValuesString(Activity activity, Param param) {
